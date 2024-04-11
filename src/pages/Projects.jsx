9c41443014316c1029de2200c73ccc6c0509b9df
RanjanKumar93 <rk0936626@gmail.com>
2024-04-11 12:31:53 +0530

    adding users project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import goto80 from "../assets/projects-img/go-to-80.png";
+import usersImg from "../assets/projects-img/users.png"
 // bg-gradient-to-br from-[#D4145A] to-[#FBB03B]
 export const Projects = () => {
   return (
@@ -18,6 +19,27 @@ export const Projects = () => {
               <a
                 target="_blank"
                 href="https://github.com/RanjanKumar93/go-to-80-game.git"
+                className="border-[2px] border-black rounded-lg px-1 hover:bg-black hover:text-white"
+              >
+                Code
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center rounded-xl py-0.5 bg-white justify-center overflow-hidden shadow-2xl flex-col w-full">
+          <img src={usersImg} alt="users-project" className="bg-cover" />
+          <div className="text-xl sm:text-2xl md:text-3xl my-2 sm:my-4">
+            <div className="flex space-x-3">
+              <a
+                target="_blank"
+                href="https://users-info-project.vercel.app/"
+                className="border-[2px] border-black rounded-lg px-1 hover:bg-black hover:text-white"
+              >
+                Live
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/RanjanKumar93/users-info-project.git"
                 className="border-[2px] border-black rounded-lg px-1 hover:bg-black hover:text-white"
               >
                 Code
