9beb35bed000bb92e7ae1483d8dfcc03d9aa32cf
RanjanKumar93 <rk0936626@gmail.com>
2024-04-16 12:03:37 +0530

    adding findCountry project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import goto80 from "../assets/projects-img/go-to-80.png";
 import usersImg from "../assets/projects-img/users.png"
+import findCountryImg from "../assets/projects-img/find-country.png"
 // bg-gradient-to-br from-[#D4145A] to-[#FBB03B]
 export const Projects = () => {
   return (
@@ -40,6 +41,27 @@ export const Projects = () => {
               <a
                 target="_blank"
                 href="https://github.com/RanjanKumar93/users-info-project.git"
+                className="border-[2px] border-black rounded-lg px-1 hover:bg-black hover:text-white"
+              >
+                Code
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center rounded-xl py-0.5 bg-white justify-center overflow-hidden shadow-2xl flex-col w-full">
+          <img src={findCountryImg} alt="find-country-project" className="bg-cover" />
+          <div className="text-xl sm:text-2xl md:text-3xl my-2 sm:my-4">
+            <div className="flex space-x-3">
+              <a
+                target="_blank"
+                href="https://find-countries-project.vercel.app/"
+                className="border-[2px] border-black rounded-lg px-1 hover:bg-black hover:text-white"
+              >
+                Live
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/RanjanKumar93/FindCountries-Project.git"
                 className="border-[2px] border-black rounded-lg px-1 hover:bg-black hover:text-white"
               >
                 Code
