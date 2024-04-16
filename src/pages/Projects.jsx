3ee4680f0e76042db340b59c95099155ddda2a7a
RanjanKumar93 <rk0936626@gmail.com>
2024-04-16 12:38:56 +0530

    css

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,7 +5,7 @@ import findCountryImg from "../assets/projects-img/find-country.png"
 export const Projects = () => {
   return (
     <div className="w-full h-[100rem] bg-gradient-to-tl from-[#868F96] to-[#596164] flex items-start justify-center">
-      <div className="grid w-full grid-cols-1 sm:grid-cols-2 py-2 md:py-8 mx-2 md:mx-10 lg:mx-20 xl:mx-40 gap-y-6 md:gap-y-20 gap-x-4">
+      <div className="grid w-full grid-cols-1 sm:grid-cols-2 py-2 md:py-8 mx-2 md:mx-10 lg:mx-20 xl:mx-40 gap-y-3 md:gap-y-20 gap-x-4">
         <div className="flex items-center rounded-xl py-0.5 bg-white justify-center overflow-hidden shadow-2xl flex-col w-full">
           <img src={goto80} alt="go-to-80-game" className="bg-cover" />
           <div className="text-xl sm:text-2xl md:text-3xl my-2 sm:my-4">
